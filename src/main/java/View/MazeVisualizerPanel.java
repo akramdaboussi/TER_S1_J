ed6d2381e5e58211e6b30e335361050fd380a668
akramdaboussi <akram.daboussi@etu.unice.fr>
2025-11-09 16:56:36 +0100

    amélioration du rendu visuel pour que ça ressemble plus à Pac-Man7

--- src/main/java/View/MazeVisualizerPanel.java
+++ src/main/java/View/MazeVisualizerPanel.java
@@ -19,19 +19,39 @@ public class MazeVisualizerPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        
+        // Active l'anti-aliasing pour des bords plus lisses
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Taille des coins arrondis pour les murs
+        int arcSize = CELL_SIZE / 2;
+
         for (int y = 0; y < mazeGrid.length; y++) {
-            for (int x = 0; x < mazeGrid[y].length; x++) {
-                // On choisit la couleur en fonction du chiffre dans la grille
+            for (int x = 0; x < mazeGrid[y].length; x++) { 
+                int cellX = x * CELL_SIZE;
+                int cellY = y * CELL_SIZE;
+                
                 switch (mazeGrid[y][x]) {
-                    case 0: g.setColor(Color.BLACK); break; // SOL
-                    case 1: g.setColor(new Color(0,0,200));; break; // MUR
-                    case 2: g.setColor(new Color(0, 0, 200)); break; // MUR_PERMANENT
-                    case 3: g.setColor(Color.BLACK); break; // GHOST_HOUSE
-                    case 4: g.setColor(Color.BLACK); break; // TUNNEL
-                    default: g.setColor(Color.RED); break;  // Erreur
+                    case 0: 
+                    case 3: // Ghost House
+                    case 4: // Tunnel
+                        g2d.setColor(Color.BLACK); 
+                        g2d.fillRect(cellX, cellY, CELL_SIZE, CELL_SIZE);
+                        break;
+
+                    case 1: // Mur
+                    case 2: // Mur permanent
+                        g2d.setColor(new Color(0,0,200)); 
+                        // Dessine des murs avec des coins arrondis (pour un rendu plus Pac-Man)
+                        g2d.fillRoundRect(cellX, cellY, CELL_SIZE, CELL_SIZE, arcSize, arcSize);
+                        break;
+                        
+                    default: 
+                        g2d.setColor(Color.RED); 
+                        g2d.fillRect(cellX, cellY, CELL_SIZE, CELL_SIZE);
+                        break;
                 }
-                // On dessine le carré
-                g.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
             }
         }
     }
