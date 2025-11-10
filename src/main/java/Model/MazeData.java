924f2e25a98cbc54e5c261fda15fd7ac360e3b3b
akramdaboussi <akram.daboussi@etu.unice.fr>
2025-11-10 11:49:42 +0100

    ajout du champ ident au modèle MazeData

--- src/main/java/Model/MazeData.java
+++ src/main/java/Model/MazeData.java
@@ -4,4 +4,4 @@ package Model;
  * Représente la structure de données simple du labyrinthe pour la conversion JSON
 */
 
-public record MazeData(int width, int height, int[][] grid) {}
+public record MazeData(String ident, int width, int height, int[][] grid) {}
